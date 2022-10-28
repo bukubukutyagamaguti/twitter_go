df9a9c6201712dd6b7af8b9a4f199b33fc74df82
bukubukutyagamaguti <bukubukutyagamaguti@gmail.com>
2022-10-28 23:38:16 +0900

    domainを作成
    各DBのtableで必要になりそうな最低限を実装

---
+++ domain/post.go
@@ -0,0 +1,12 @@
+package domain
+
+import "database/sql"
+
+type Posts []Post
+type Post struct {
+	Id        int `json:"id" param:"id" gorm:"primaryKey"`
+	UserId    int `json:"user_id" validate:"required" gorm:"column:user_id"`
+	Message   string       `json:"message" validate:"required" gorm:"column:message"`
+	DeletedAt sql.NullTime `json:"deleted_at" validate:"deleted_at" gorm:"column:deleted_at"`
+	CreatedAt string       `json:"created_at" validate:"required,created_at" gorm:"column:created_at"`
+}
